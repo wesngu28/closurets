4edc612da0c3ecb7608efc3188413fae1540ae5a
Wesley <serpents123@gmail.com>
2022-06-15 20:33:26 -0700

    Bot client and handlers

--- client/Closure.ts
+++ client/Closure.ts
@@ -1,6 +1,10 @@
 import DiscordJS from 'discord.js';
 import fs from 'fs';
+import dotenv from 'dotenv';
 import path from 'node:path';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 export class Closure extends DiscordJS.Client {
   public commands: DiscordJS.Collection<string, any>;
@@ -13,6 +17,7 @@ export class Closure extends DiscordJS.Client {
   start() {
     this.commandHandler();
     this.eventHandler();
+    this.deployCommands();
     this.login(process.env.TOKEN);
   }
 
@@ -39,6 +44,26 @@ export class Closure extends DiscordJS.Client {
         this.on(event.name, (...args) => event.execute(...args));
       }
     }
+  }
+
+  async deployCommands() {
+    const commands = [];
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter((file:any) => file.endsWith('.ts'));
+
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
+      commands.push(command.data.toJSON());
+    }
+
+    console.log(process.env.TOKEN);
+
+    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN!);
+
+    rest.put(Routes.applicationCommands(process.env.CID!), { body: commands })
+      .then(() => console.log('Successfully registered application commands.'))
+      .catch(console.error);
   }
 
 }
