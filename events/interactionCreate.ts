099403fc0bbcb69a3ce6deb36b0af098f037d50e
Wesley <serpents123@gmail.com>
2022-06-27 18:59:20 -0700

    per guild cooldown

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -8,19 +8,19 @@ export const interactionCreate = async (client: Closure, interaction: Interactio
 	const command = client.commands.get(interaction.commandName);
   const commandName = Object.keys(command)[0];
 	if (!command) return;
-  if (commandCooldown.has(`${interaction.commandName}.${interaction.user.id}`)) {
+  if (commandCooldown.has(`${interaction.guild}.${interaction.commandName}.${interaction.user.id}`)) {
     interaction.reply({ content: 'Please wait for ten seconds after using this command before trying again', ephemeral: true });
     return;
   }
 
 	try {
 		await command[commandName].execute(interaction);
-		commandCooldown.add(`${interaction.commandName}.${interaction.user.id}`);
+		commandCooldown.add(`${interaction.guild}.${interaction.commandName}.${interaction.user.id}`);
 		setTimeout(() => {
-			commandCooldown.delete(`${interaction.commandName}.${interaction.user.id}`);
+			commandCooldown.delete(`${interaction.guild}.${interaction.commandName}.${interaction.user.id}`);
 		}, 10000);
 	} catch (error) {
-		commandCooldown.delete(`${interaction.commandName}.${interaction.user.id}`);
+		commandCooldown.delete(`${interaction.guild}.${interaction.commandName}.${interaction.user.id}`);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 
