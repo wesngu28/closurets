11f0d6c40c5b6507dd12cd4d4e95e639be158183
Wesley <serpents123@gmail.com>
2022-07-06 15:39:43 -0700

    EmbedInformation typing and publishedat

--- src/types/AnnouncementEmbed.ts
+++ src/types/AnnouncementEmbed.ts
@@ -1,6 +1,19 @@
 import { MessageEmbed } from 'discord.js';
 
+export interface EmbedInformation {
+  title: string;
+  name: string;
+  iconURL: string;
+  url: string;
+  description: string;
+  thumbnail: string;
+  image: string;
+  id: string;
+  publishedAt: string;
+}
+
 export interface AnnouncementEmbed {
   content: string;
   embeds: MessageEmbed[];
+  publishedDate: string;
 }
