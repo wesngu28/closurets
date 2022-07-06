0a47b613a540eea7ca67e54cb2e8cc072c020003
Wesley <serpents123@gmail.com>
2022-07-06 15:23:21 -0700

    Use fillCollection, handle error separately

--- src/commands/tracker.ts
+++ src/commands/tracker.ts
@@ -1,9 +1,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { GuildMember, Interaction } from 'discord.js';
-import { getIDFromLink } from '../../lib/youtube/getIDFromLink';
 import trackerModel from '../models/trackerModel';
 import { Command } from '../types/Command';
 import { Tracker } from '../types/Tracker';
+import { fillChannelCollection } from './helpers/youtube/fillChannelCollection';
+import { getIDFromLink } from './helpers/youtube/getIDFromLink';
 
 export const configureTracker: Command = {
   data: new SlashCommandBuilder()
@@ -16,40 +17,64 @@ export const configureTracker: Command = {
         .setRequired(true)
     ),
   async execute(interaction: Interaction) {
-    if (!interaction.isCommand()) {
-      return;
-    }
-    const member = interaction.member as GuildMember;
-    if (member!.permissions.has('ADMINISTRATOR') === false) {
-      await interaction.reply({
-        content: 'You are not able to execute this command!',
-        ephemeral: true,
-      });
-      return;
-    }
-    const tracked = await trackerModel.findOne({ _id: interaction.guild!.id });
-    if (tracked) {
-      tracked.channelID = interaction.channel!.id;
-      const inputChannel = interaction.options.getString('channel')!;
-      const id = await getIDFromLink(inputChannel);
-      if (id === 'You provided an invalid link') {
+    try {
+      if (!interaction.isCommand()) {
         return;
       }
-      tracked.ytID = id;
-      await tracked.save();
-      interaction.reply({ content: `Now tracking activity from ${tracked.ytID}`, ephemeral: true });
-    } else {
+      const member = interaction.member as GuildMember;
+      if (member!.permissions.has('ADMINISTRATOR') === false) {
+        await interaction.reply({
+          content: 'You are not able to execute this command!',
+          ephemeral: true,
+        });
+        return;
+      }
+      const tracked = await trackerModel.findOne({ _id: interaction.guild!.id });
+      if (tracked) {
+        tracked.channelID = interaction.channel!.id;
+        const inputChannel = interaction.options.getString('channel')!;
+        if (inputChannel === tracked.ytID) {
+          interaction.reply({
+            content: 'You provided an already tracked channel',
+            ephemeral: true,
+          });
+          return;
+        }
+        const id = await getIDFromLink(inputChannel);
+        if (id === 'You provided an invalid link') return;
+        tracked.ytID = id;
+        await tracked.save();
+        interaction.reply({
+          content: `Now tracking activity from ${tracked.ytID}`,
+          ephemeral: true,
+        });
+        const announceable = await fillChannelCollection(interaction.guild!.id, id);
+        if (announceable && interaction.channel?.isText()) {
+          interaction.channel.send(announceable);
+        }
+      }
       const trackerObject: Tracker = {
         _id: interaction.guild!.id,
         channelID: interaction.channel!.id,
       };
       const inputChannel = interaction.options.getString('channel')!;
-      trackerObject.ytID = await getIDFromLink(inputChannel);
+      const id = await getIDFromLink(inputChannel);
+      if (id === 'You provided an invalid link') return;
+      trackerObject.ytID = id;
       await trackerModel.create(trackerObject);
       interaction.reply({
         content: `Now tracking activity from ${trackerObject.ytID}`,
         ephemeral: true,
       });
+      const announceable = await fillChannelCollection(interaction.guild!.id, id);
+      if (announceable && interaction.channel?.isText()) interaction.channel.send(announceable);
+    } catch (err) {
+      if (interaction.isCommand()) {
+        await interaction.reply({
+          content: 'There was an error while executing this command!',
+          ephemeral: true,
+        });
+      }
     }
   },
 };
