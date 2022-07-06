d13bd3ee4c0db74db18173c81fc82b2b4d0a61e6
Wesley <serpents123@gmail.com>
2022-07-06 15:39:19 -0700

    separate date and more informative log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,15 +1,15 @@
-import { fetchLiveStream } from '../../lib/youtube/fetchLiveStream';
 import { Closure } from '../client/Closure';
+import { fetchLiveStream } from '../commands/helpers/youtube/fetchLiveStream';
 import trackerModel from '../models/trackerModel';
 import { DiscordEvent } from '../types/DiscordEvent';
-
-const currentDate = new Date(Date.now()).toISOString();
 
 export const ready: DiscordEvent = {
   name: 'ready',
   async execute(client: Closure): Promise<void> {
     if (!client.user) return;
-    console.log(`Bot ${client.user.tag} is logged in!`);
+    console.log(
+      `Bot ${client.user.tag} is logged in at ${client.runDate} for ${process.env.NODE_ENV}!`
+    );
     client.user.setPresence({ activities: [{ name: 'Selling Originite Prime' }] });
     try {
       setInterval(() => {
@@ -18,7 +18,7 @@ export const ready: DiscordEvent = {
           const tracked = await trackerModel.findOne({ _id: guild });
           if (tracked !== null) {
             const channel = client.channels.cache.get(tracked.channelID);
-            fetchLiveStream(guild, tracked.ytID!, currentDate).then(async data => {
+            fetchLiveStream(guild, tracked.ytID!, client.runDate).then(async data => {
               if (data) {
                 if (channel) {
                   if (channel.isText()) {
