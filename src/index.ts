021119c8a70ce12d39858e347af6607b2a49d979
Wesley <serpents123@gmail.com>
2022-07-06 13:59:39 -0700

    Instantiate bot

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv';
+import DiscordJS from 'discord.js';
 import { Closure } from './client/Closure';
 import { databaseConnect } from './models/connect';
 import { eventHandler } from './client/eventHandler';
 import { commandHandler, deployCommands } from './client/commandHandler';
+import { Command } from './types/Command';
 
 dotenv.config();
 
 databaseConnect();
-const client = new Closure();
+const client = new Closure(
+  new DiscordJS.Collection<string, Command>(),
+  new Date(Date.now()).toISOString(),
+  new Set<string>()
+);
 
 async function buildClient(bot: Closure) {
   await eventHandler(bot);
