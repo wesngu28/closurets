070c2aaed3398bfafc962f08189f627d4c762770
Wesley <serpents123@gmail.com>
2022-07-06 15:27:30 -0700

    Defer earlier to account for heroku cold start

--- src/commands/operator.ts
+++ src/commands/operator.ts
@@ -1,11 +1,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Interaction } from 'discord.js';
-import { assembleButtons } from '../../lib/embed/assembleButtons';
-import { formulateResponse } from '../../lib/embed/formulateResponse';
-import { skinPaginator } from '../../lib/embed/skinPaginator';
-import { getOperatorData } from '../../lib/rhodes/getOperatorData';
 import { Command } from '../types/Command';
 import { Operator } from '../types/Operator';
+import { assembleButtons } from './helpers/operator/assembleButtons';
+import { formulateResponse } from './helpers/operator/formulateResponse';
+import { getOperatorData } from './helpers/operator/getOperatorData';
+import { skinPaginator } from './helpers/operator/skinPaginator';
 
 export const operator: Command = {
   data: new SlashCommandBuilder()
@@ -15,21 +15,33 @@ export const operator: Command = {
       option.setName('name').setDescription('Rhodes Island Operator').setRequired(true)
     ),
   async execute(interaction: Interaction) {
-    if (interaction.isCommand()) {
-      let name = interaction.options.getString('name');
-      name = name!.replaceAll(' ', '-');
-      const data: Operator | { error: 'Operator not found' } = await getOperatorData(name);
-      if (Object.keys(data).includes('error')) {
+    try {
+      if (interaction.isCommand()) {
+        await interaction.deferReply();
+        let name = interaction.options.getString('name');
+        name = name!.replaceAll(' ', '-');
+        const data: Operator | { error: 'Operator not found' } = await getOperatorData(name);
+        if (Object.keys(data).includes('error')) {
+          await interaction.deleteReply();
+          await interaction.followUp({
+            content: 'Something went wrong with the operator name you specified.',
+            ephemeral: true,
+          });
+        } else {
+          const imgList: { [key: string]: string } = (data as Operator).art;
+          const buttons = assembleButtons(imgList);
+          const embed = formulateResponse(data as Operator);
+          const timeout = 120000;
+          await skinPaginator(interaction, embed, buttons, imgList, timeout);
+        }
+      }
+    } catch (err) {
+      if (interaction.isCommand()) {
+        await interaction.deleteReply();
         await interaction.reply({
-          content: 'Something went wrong with the operator name you specified.',
+          content: 'There was an error while executing this command!',
           ephemeral: true,
         });
-      } else {
-        const imgList: { [key: string]: string } = (data as Operator).art;
-        const buttons = assembleButtons(imgList);
-        const embed = formulateResponse(data as Operator);
-        const timeout = 120000;
-        await skinPaginator(interaction, embed, buttons, imgList, timeout);
       }
     }
   },
