b5c71905ce75219f004e1a11c1267d6d5aa3726e
Wesley <serpents123@gmail.com>
2022-07-09 15:54:33 -0700

    connect to redis only once

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { databaseConnect } from './models/connect';
 import { eventHandler } from './client/eventHandler';
 import { commandHandler, deployCommands } from './client/commandHandler';
 import { Command } from './types/Command';
+import { RedisClient } from './models/redis';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ async function buildClient(bot: Closure) {
   await eventHandler(bot);
   await commandHandler(bot);
   await deployCommands();
+  await RedisClient.init();
   bot.login(process.env.TOKEN);
 }
 
