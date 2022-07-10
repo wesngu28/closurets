ab12e9ceb7b7e8d722d42d42a53b64b742d4f866
Wesley <serpents123@gmail.com>
2022-07-10 14:01:15 -0700

    Website files

---
+++ frontend/src/scripts.ts
@@ -0,0 +1,32 @@
+const button = document.querySelector('#link')!;
+button.addEventListener('click', () => {
+  window.open(
+    'https://discord.com/api/oauth2/authorize?client_id=284432080069459971&permissions=294205483072&scope=applications.commands%20bot',
+    '_blank'
+  );
+});
+
+let slideIndex = 0;
+
+const carousel = () => {
+  const x = document.getElementsByClassName('liveSlide');
+  const y = document.getElementsByClassName('trackSlide');
+  const z = document.getElementsByClassName('opSlide');
+  if (x.length === y.length && x.length === z.length) {
+    for (let i = 0; i < x.length; i += 1) {
+      x[i].classList.add('hidden');
+      y[i].classList.add('hidden');
+      z[i].classList.add('hidden');
+    }
+    slideIndex += 1;
+    if (slideIndex > x.length) {
+      slideIndex = 1;
+    }
+    x[slideIndex - 1].classList.remove('hidden');
+    y[slideIndex - 1].classList.remove('hidden');
+    z[slideIndex - 1].classList.remove('hidden');
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+  }
+};
+
+carousel();
