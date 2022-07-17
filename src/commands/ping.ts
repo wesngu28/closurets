8728d0c7d3036a1c8bc687bb0bb8b88179730e36
Wesley <serpents123@gmail.com>
2022-07-16 19:23:13 -0700

    ping

---
+++ src/commands/ping.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Interaction } from 'discord.js';
+import { deleteAndFollowUp } from '../helpers/deleteAndFollowUp';
+import { Command } from '../types/Command';
+
+export const ping: Command = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Get both the websocket heartbeat and roundtrip latency of the bot.'),
+  async execute(interaction: Interaction) {
+    try {
+      if (!interaction.isCommand()) return;
+      const wsHeartBeat = interaction.client.ws.ping;
+      const roundTrip = Math.abs(Date.now() - interaction.createdTimestamp);
+      await interaction.reply(
+        `Websocket heartbeat: ${wsHeartBeat}ms, roundtrip latency: ${roundTrip}ms`
+      );
+    } catch (err) {
+      if (!interaction.isCommand()) return;
+      deleteAndFollowUp(interaction, 'There was an error while executing this command!');
+    }
+  },
+};
